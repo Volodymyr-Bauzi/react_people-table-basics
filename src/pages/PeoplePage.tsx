35df3ba6df4462919caad28203be2003b50f1a10
Bauzi <volodiasyd@gmail.com>
2025-11-09 01:05:45 +0200

    fix: improve loading state handling and conditional rendering in PeoplePage component

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -28,27 +28,33 @@ const PeoplePage = () => {
     );
   }, [people]);
 
-  if (isLoading) {
-    return <Loader />;
-  }
-
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="box table-container">
-          {people.length > 0 && <PeopleTable people={people} byName={byName} />}
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {people.length === 0 && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-          {people.length === 0 && (
-            <p data-cy="noPeopleMessage">There are no people on the server</p>
-          )}
+              {people.length > 0 && (
+                <PeopleTable people={people} byName={byName} />
+              )}
 
-          {/* Only show loading error after loading finished and an error actually occurred */}
-          {!isLoading && error && (
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
+              {/* Only show loading error after loading finished and an error actually occurred */}
+              {!isLoading && error && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
+            </>
           )}
         </div>
       </div>
